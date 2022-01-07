ca72b39ce46beafaf17116f47944c65570ee6c3e
linawincent <90678895+linawincent@users.noreply.github.com>
2022-01-07 13:46:08 +0100

    Add files via upload

--- 6dim.c
+++ 6dim.c
@@ -4,42 +4,39 @@
 #include <math.h>
 #include <time.h>
 #include "DMC.h"
-#include "DMC.c"
 #include "DMC_6dim.c"
 
 
-int main(){
-    /*
+int dim6(){
+    
     int I = 20000;
     int i0 = 10000;
     int Nmax = 2000;
     int* N = malloc((I+1) * sizeof(int));
     
-    double* x[Nmax];
+    double* pos[Nmax][6];
     
     for (int j=0; j<Nmax; j++) {
-        x[j] = (double*)malloc(I * sizeof(double));
-    }*/
-
-    int I = 200;
-    int i0 = 10000;
-    int Nmax = 10000;
-    int* N = malloc((I+1) * sizeof(int));
+        pos[j] = (double*)malloc(6 * sizeof(double));
+    }
     
-    double* pos = malloc(6 * (I+1) * (I+1) * sizeof(double));
+    int count = 0;
+    for (int j = 0; j < Nmax; j++)
+            for (int i = 0; i < (I+1); i++)
+                pos[j][i] = ++count;
+    
     double* ET = malloc((I+1) * sizeof(double));
     double* ET_avg = malloc((I+1) * sizeof(double));
     double* ET_avg_acc = malloc((I+1) * sizeof(double));
     
-    double dtau = 0.02;
+    double dtau = 0.01;
     double min = -5.;
     double max = 5.;
     double gamma = 0.5;
     
     srand((unsigned int)time(NULL));
     
-    // DMC(I, i0, N, Nmax, x, ET, ET_avg, ET_avg_acc, dtau, min, max, gamma);
-    DMC_6dim(I, i0, N, Nmax, pos, ET, ET_avg, ET_avg_acc, dtau, min, max, gamma);
+    DMC(I, i0, N, Nmax, pos, ET, ET_avg, ET_avg_acc, dtau, min, max, gamma);
     
     FILE *fp;
     fp = fopen("DMC.csv", "wb");
@@ -47,11 +44,11 @@ int main(){
         fprintf(fp, "%f,%f,%f\n", ET[i], ET_avg[i], ET_avg_acc[i]);
     }
     fclose(fp);
+
     FILE *f2;
     f2 = fopen("x.csv", "wb");
     for (int j = 0; j < N[I]; j++){
-        // fprintf(f2, "%f\n", pos[j][I]);
-        fprintf(f2, "%f,%f,%f,%f,%f,%f\n", pos[pos_idx(I,j,0,I,N[I])],pos[pos_idx(I,j,1,I,N[I])], pos[pos_idx(I,j,2,I,N[I])], pos[pos_idx(I,j,3,I,N[I])], pos[pos_idx(I,j,4,I,N[I])], pos[pos_idx(I,j,5,I,N[I])]);
+        fprintf(f2, "%f,%f,%f,%f,%f,%f\n", pos[j][0],pos[j][1], pos[j][2], pos[j][3], pos[j][4], pos[j][5]);
     }
     fclose(f2);
     return 0;
