ae8a9de4f624f2b262f7eb4ebc116fc6133f16a0
vicorGUS <79367388+vicorGUS@users.noreply.github.com>
2022-01-10 13:50:31 +0100

    Add files via upload

--- DMC_6dim.c
+++ DMC_6dim.c
@@ -9,40 +9,38 @@ int pos_idx(int i, int j, int k, int I, int N){
     return (i * 6 * N) + (j * 6) + k;
 }
 
-void DMC_6dim(int I, int i0, int* N, int Nmax, double* pos, double* ET, double* ET_avg, double* ET_avg_acc, double dtau, double gamma){
+void DMC_6dim(int I, int i0, int* N, int Nmax, double* pos, double* ET, double* ET_avg, double dtau, double gamma){
 
-    int m = 0;
+    int m;
     double* V = malloc(Nmax * sizeof(double));
     double* W = malloc(Nmax * sizeof(double));
     double r1 = 0.7 + (double) rand() / RAND_MAX;
-    
     double r2 = 0.7 + (double) rand() / RAND_MAX;
-    printf("r1: %f r2: %f \n", r1, r2);
-    double t1 = acos(2. * rand() / RAND_MAX - 1);
-    double t2 = acos(2. * rand() / RAND_MAX - 1);
-    double p1 = 2. * 3.14 * rand() / RAND_MAX;
-    double p2 = 2. * 3.14 * rand() / RAND_MAX;
+    double r12;
+    double t1 = acos(2. * (double) rand() / RAND_MAX - 1);
+    double t2 = acos(2. * (double) rand() / RAND_MAX - 1);
+    double p1 = 2. * 3.14 * (double) rand() / RAND_MAX;
+    double p2 = 2. * 3.14 *  (double) rand() / RAND_MAX;
     
-    for (int i=0; i<(I+1); i++) {
+    for (int i=1; i<(I+1); i++) {
         N[i] = 0;
     }
     
-    N[0] = 6000;
+    N[0] = 1000;
     ET[0] = -3.0;
-    
 
     for (int j=0; j<N[0]; j++){
-        pos[pos_idx(0,j,0,I,N[I])] = (double) r1 * sin(t1) * cos(p1); //x1
-        pos[pos_idx(0,j,1,I,N[I])] = (double) r2 * sin(t1) * sin(p1); //y1
-        pos[pos_idx(0,j,2,I,N[I])] = (double) r1 * cos(t1);           //z1
-        pos[pos_idx(0,j,3,I,N[I])] = (double) r2 * sin(t2) * cos(p2); //x2
-        pos[pos_idx(0,j,4,I,N[I])] = (double) r2 * sin(t2) * sin(p2); //y2
-        pos[pos_idx(0,j,5,I,N[I])] = (double) r2 * cos(t2);           //z2 
+        pos[pos_idx(0,j,0,I,Nmax)] = (double) r1 * sin(t1) * cos(p1); //x1
+        pos[pos_idx(0,j,1,I,Nmax)] = (double) r1 * sin(t1) * sin(p1); //y1
+        pos[pos_idx(0,j,2,I,Nmax)] = (double) r1 * cos(t1);           //z1
+        pos[pos_idx(0,j,3,I,Nmax)] = (double) r2 * sin(t2) * cos(p2); //x2
+        pos[pos_idx(0,j,4,I,Nmax)] = (double) r2 * sin(t2) * sin(p2); //y2
+        pos[pos_idx(0,j,5,I,Nmax)] = (double) r2 * cos(t2);           //z2
     }
     
     for (int j=N[0]; j<Nmax; j++ ) {
-        for (int k=0; k<Nmax; k++ ){
-            pos[pos_idx(0,j,k,I,N[I])] = 0.0;
+        for (int k=0; k<6; k++ ){
+            pos[pos_idx(0,j,k,I,Nmax)] = 0.0;
         }
     }
     
@@ -50,46 +48,51 @@ void DMC_6dim(int I, int i0, int* N, int Nmax, double* pos, double* ET, double* 
     
     for (int i=0; i<I; i++){
         ET_avg[i] = 0;
-        if (N[i]<Nmax) {
-            for (int j=0; j<N[i]; j++){
-                for (int k = 0; k<6; k++){
+        for (int j=0; j<N[i]; j++){
+            for (int k = 0; k<6; k++){
+                pos[pos_idx(i,j,k,I,Nmax)] += sqrt(dtau) * randn(0., 1.);
+            }
                     
-                    pos[pos_idx(i,j,k,I,N[I])] += sqrt(dtau) * randn(0., 1.);
+            r1 = sqrt(pow(pos[pos_idx(i,j,0,I,Nmax)], 2) + pow(pos[pos_idx(i,j,1,I,Nmax)], 2) + pow(pos[pos_idx(i,j,2,I,Nmax)], 2));
+                    
+            r2 = sqrt(pow(pos[pos_idx(i,j,3,I,Nmax)], 2) + pow(pos[pos_idx(i,j,4,I,Nmax)], 2) + pow(pos[pos_idx(i,j,5,I,Nmax)], 2));
+            
+            r12 = sqrt(pow(pos[pos_idx(i,j,3,I,Nmax)] - pos[pos_idx(i,j,0,I,Nmax)], 2) + pow(pos[pos_idx(i,j,4,I,Nmax)] - pos[pos_idx(i,j,1,I,Nmax)], 2) + pow(pos[pos_idx(i,j,5,I,Nmax)] - pos[pos_idx(i,j,2,I,Nmax)], 2));
+                    
+            V[j] = (-2./r1 - 2./r2 + 1./r12);
+                    
+            W[j] = exp( -(V[j] - ET[i]) * dtau);
+
+            m = (int)(W[j] + ((double) rand() / (RAND_MAX)));
+
+            N[i+1] += m;
+            
+            for (int k = 0; k<6; k++){
+                for (int l=(N[i+1]-m); l<N[i+1]; l++){
+                    pos[pos_idx(i+1,l,k,I,Nmax)] = pos[pos_idx(i,j,k,I,Nmax)];
                 }
-                    
-                r1 = sqrt(pow(pos[pos_idx(i,j,0,I,N[I])], 2) + pow(pos[pos_idx(i,j,1,I,N[I])], 2) + pow(pos[pos_idx(i,j,2,I,N[I])], 2));
-                    
-                r2 = sqrt(pow(pos[pos_idx(i,j,3,I,N[I])], 2) + pow(pos[pos_idx(i,j,4,I,N[I])], 2) + pow(pos[pos_idx(i,j,5,I,N[I])], 2));
-                    
-                V[j] = -2./r1 - 2./r2 + 1./fabs(r1 - r2);
-                    
-                W[j] = exp(- (V[j] - ET[i]) * dtau);
-
-                m = (int)(W[j] + ((double) rand() / (RAND_MAX)));
-
-                N[i+1] += m;
-                        
-//                    if (N[i+1] < Nmax){
-//                        for (int k=(N[i+1]-m); k<N[i+1]; k++){
-//                            pos[pos_idx(i+1,j,k,I,N[I])] = pos[pos_idx(i,j,k,I,N[I])];
-//                        }
-//                    }
-            
-            }
-
-            for (int i´=(i0+1); i´<i; i´++) {
-                ET_avg[i] += 1. / (i-i0) * ET[i´];
             }
             
-            ET[i+1] = ET_avg[i] - gamma * log((double)N[i] / N[0]);
-            
-            if ((i-i0+1) != 0) {
-                ET_avg[i+1] = 1 / (i - i0 + 1.) * ET[i+1] + (i - i0) / (i - i0 + 1.) * ET_avg[i];
-            }
-            
-            ET_avg_acc[i] = ET_avg[i+1];
-           
-            printf("i: %d N: %d E: %f\n",i, N[i+1], ET_avg[i+1]);
         }
+        
+        if (N[i+1]>Nmax) {
+            N[i+1] = (int) floor(N[i+1] / 2.);
+            printf("%d\n", N[i+1]);
+        }
+
+        for (int i´=0; i´<i; i´++) {
+            ET_avg[i] += (1. / i) * ET[i´];
+        }
+        
+        ET_avg[0] = -3.0;
+            
+        ET[i+1] = ET_avg[i] - gamma * log((double)N[i] / N[0]);
+        
+        if ((i-i0+1) != 0) {
+            ET_avg[i+1] = 1 / (i - i0 + 1.) * ET[i+1] + (i - i0) / (i - i0 + 1.) * ET_avg[i];
+            }
+        
+//        printf("i: %d N: %d E: %f\n",i, N[i], ET_avg[i]);
     }
+    printf("N: %d E: %f\n",N[I], ET_avg[I]);
 }
--- main.c
+++ main.c
@@ -4,55 +4,58 @@
 #include <math.h>
 #include <time.h>
 #include "DMC.h"
-#include "DMC.c"
-#include "DMC_6dim.c"
-
 
 int main(){
-    /*
-    int I = 20000;
-    int i0 = 10000;
-    int Nmax = 2000;
-    int* N = malloc((I+1) * sizeof(int));
     
-    double* x[Nmax];
+    int I = 85000;
+    int i0 = 0;
+    int Nmax = 4000;
     
-    for (int j=0; j<Nmax; j++) {
-        x[j] = (double*)malloc(I * sizeof(double));
-    }*/
-
-    int I = 200;
-    int i0 = 10000;
-    int Nmax = 10000;
-    int* N = malloc((I+1) * sizeof(int));
+//    double* x[Nmax];
+//
+//    for (int j=0; j<Nmax; j++) {
+//        x[j] = (double*)malloc(I * sizeof(double));
+//    }
+//
+//    int count = 0;
+//    for (int j = 0; j < Nmax; j++)
+//            for (int i = 0; i < (I+1); i++)
+//                x[j][i] = ++count;
     
-    double* pos = malloc(6 * (I+1) * (I+1) * sizeof(double));
     double* ET = malloc((I+1) * sizeof(double));
     double* ET_avg = malloc((I+1) * sizeof(double));
-    double* ET_avg_acc = malloc((I+1) * sizeof(double));
+
+    double* pos = malloc(6 * (I+1) * Nmax * sizeof(double));
+    int* N = malloc((I+1) * sizeof(int));
     
-    double dtau = 0.02;
-    double min = -5.;
-    double max = 5.;
+    double dtau = 0.01;
     double gamma = 0.5;
     
     srand((unsigned int)time(NULL));
     
-    // DMC(I, i0, N, Nmax, x, ET, ET_avg, ET_avg_acc, dtau, min, max, gamma);
-    DMC_6dim(I, i0, N, Nmax, pos, ET, ET_avg, ET_avg_acc, dtau, min, max, gamma);
+    //DMC(I, i0, N, Nmax, x, ET, ET_avg, dtau, gamma);
     
-    FILE *fp;
-    fp = fopen("DMC.csv", "wb");
+    DMC_6dim(I, i0, N, Nmax, pos, ET, ET_avg, dtau, gamma);
+    
+//    FILE *fp;
+//    fp = fopen("DMC.csv", "wb");
+//    for (int i=0; i<I; i++) {
+//        fprintf(fp, "%f, %f, %d\n", ET_avg[i], ET[i], N[i]);
+//    }
+//    fclose(fp);
+//
+//    FILE *f;
+//    f = fopen("x.csv", "wb");
+//    for (int j=0; j<N[I]; j++) {
+//        fprintf(f, "%f\n", x[j][I]);
+//    }
+//    fclose(f);
+    
+    FILE * f;
+    f = fopen("DMC_6dim.csv", "wb");
     for (int i=0; i<I; i++) {
-        fprintf(fp, "%f,%f,%f\n", ET[i], ET_avg[i], ET_avg_acc[i]);
+        fprintf(f, "%f, %d\n", ET_avg[i], N[i]);
     }
-    fclose(fp);
-    FILE *f2;
-    f2 = fopen("x.csv", "wb");
-    for (int j = 0; j < N[I]; j++){
-        // fprintf(f2, "%f\n", pos[j][I]);
-        fprintf(f2, "%f,%f,%f,%f,%f,%f\n", pos[pos_idx(I,j,0,I,N[I])],pos[pos_idx(I,j,1,I,N[I])], pos[pos_idx(I,j,2,I,N[I])], pos[pos_idx(I,j,3,I,N[I])], pos[pos_idx(I,j,4,I,N[I])], pos[pos_idx(I,j,5,I,N[I])]);
-    }
-    fclose(f2);
+    fclose(f);
     return 0;
 }
