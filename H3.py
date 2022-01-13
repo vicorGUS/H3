d5304652fc73e68a576af43ae8b7ee0cb4173f6d
vicorGUS <79367388+vicorGUS@users.noreply.github.com>
2022-01-13 15:18:47 +0100

    Add files via upload

--- H3.py
+++ H3.py
@@ -8,21 +8,34 @@ E6 = np.genfromtxt('DMC_6dim.csv', delimiter=',')
 
 """t = np.linspace(0, len(E[:, 0]), len(E[:, 0]))
 
-plt.plot(t[:], E[:, 2], label='\u0394\u03C4 = 0.02')
-plt.plot(t[:], 200 * np.ones(len(t[:])), 'r--', label='N0=200')
+plt.plot(t[:2500]*0.02, E[:2500, 2], label='\u0394\u03C4 = 0.02')
+plt.plot(t[:2500]*0.02, 200 * np.ones(len(t[:2500])), 'r--', label='N0=200')
 
-plt.xlabel("Iterations")
+plt.xlabel('\u03C4')
 plt.ylabel("Number of walkers")
 plt.legend(loc='best')
-plt.savefig("Walkers_full.jpg")
-"""
-"""plt.plot(t[:2500], E[:2500, 1], label='\u0394\u03C4 = 0.02')
-plt.plot(t[:2500], 0.375 * np.ones(len(t[:2500])), 'r--', label='Analytical result (E=0.375)')
+plt.savefig("Walkers_cut.jpg")
+plt.show()"""
 
-plt.xlabel("Iterations")
-plt.ylabel('Energy (Ha)')
+"""plt.plot(t[:2500]*0.02, E[:2500, 0], label='\u0394\u03C4 = 0.02')
+plt.plot(t[:2500]*0.02, 0.375 * np.ones(len(t[:2500])), 'r--', label='Analytical result (E=0.375)')
+a = np.std(E[:, 1])
+plt.fill_between(t[:2500]*0.02, E[:2500, 0]-a, E[:2500, 0]+a, alpha=0.3, label=r'$\langle E_T \rangle\pm$ one standard deviation')
+
+plt.xlabel('\u03C4')
+plt.ylabel(r'$\langle E_T \rangle$ (Ha)')
 plt.legend(loc='best')
-plt.savefig("Energy_cut.jpg")"""
+plt.savefig("Energy_avg_cut.jpg")
+plt.show()"""
+
+"""plt.plot(t[:2500]*0.02, E[:2500, 1], label='\u0394\u03C4 = 0.02')
+plt.plot(t[:2500]*0.02, 0.375 * np.ones(len(t[:2500])), 'r--', label='Analytical result (E=0.375)')
+
+plt.xlabel('\u03C4')
+plt.ylabel(r'$E_T$ (Ha)')
+plt.legend(loc='best')
+plt.savefig("Energy_cut.jpg")
+plt.show()"""
 
 """nb = 20
 
@@ -33,7 +46,8 @@ s = (b - a) / nb
 N = np.zeros(nb)
 
 for i in range(nb):
-    for j in range(1000):
+    print(i)
+    for j in range(7500):
         for x in xs[:, j]:
             if a + i * s < x <= a + (i+1) * s:
                 N[i] += 1
@@ -53,19 +67,17 @@ plt.savefig('Wave_function.jpg')
 
 plt.show()"""
 
-"""plt.figure(figsize=(8, 6))
+plt.figure(figsize=(8, 6))
 t = np.linspace(0, len(E6[:, 0]), len(E6[:, 0]))
-plt.plot(t[:], E6[:, 0], label='\u0394\u03C4 = 0.01')
+plt.plot(t[:], E6[:, 1], label='\u0394\u03C4 = 0.01')
 plt.plot(t[:], -2.903*np.ones(len(t)), 'r--', label='Analytical result (E=-2.903)')
 
 plt.xlabel('Iterations')
 plt.ylabel(r'$\langle E_T \rangle$  (a.u.)')
 plt.legend(loc='best')
 
-print(np.mean(E6[60000:, 0]))
-
-plt.savefig('Energy_6dim.jpg')"""
-
+#plt.savefig('Energy_6dim.jpg')
+plt.figure(figsize=(8, 6))
 t = np.linspace(0, len(E6[:, 0]), len(E6[:, 0]))
 plt.plot(t[:], E6[:, 2], label='\u0394\u03C4 = 0.01')
 plt.plot(t[:], 1000*np.ones(len(t)), 'r--', label='N0=1000')
@@ -74,5 +86,5 @@ plt.xlabel('Iterations')
 plt.ylabel('Number of walkers')
 plt.legend(loc='best')
 
-plt.savefig('Walkers_6dim.jpg')
+#plt.savefig('Walkers_6dim.jpg')
 plt.show()
